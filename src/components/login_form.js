96f839007824adc50363a126a4883bc4f5b6ba45
ManhND <manhnd.hebestorm@gmail.com>
2020-09-20 12:52:23 +0700

    Login

---
+++ src/components/login_form.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import { Form, Button, Card, Toast } from 'react-bootstrap'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+
+export default class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+    loggedIn: false,
+  }
+
+  login = () => {
+    axios
+      .post('/api/v1/users/login', {
+        user: {
+          email: this.state.email,
+          password: this.state.password,
+        },
+      })
+      .then((response) => {
+        this.setState({ loggedIn: true })
+        alert('Login Sucess')
+        console.log(response.data)
+      })
+      .catch((error) => {
+        if (error.response) {
+          if (error.response.status === 401) {
+            alert('Wrong email or password')
+          }
+        }
+
+        console.log(error)
+      })
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value })
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({ password: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loggedIn ? <Redirect to='/notes' /> : null}
+
+        <Card style={{ width: '18rem', align: 'center' }}>
+          <Card.Header>Login</Card.Header>
+          <Card.Body>
+            <Form>
+              <Form.Group controlId='formBasicEmail'>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type='email'
+                  placeholder='Enter email'
+                  onChange={this.handleEmailChange}
+                />
+                <Form.Text className='text-muted'>
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId='formBasicPassword'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type='password'
+                  placeholder='Password'
+                  onChange={this.handlePasswordChange}
+                />
+              </Form.Group>
+              <Button variant='primary' onClick={() => this.login()}>
+                Submit
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  }
+}
---
+++ src/layouts/navigation_bar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Navbar } from 'react-bootstrap'
+
+export default class NavigationBar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar>
+          <Navbar.Brand href='#home'>Google Kept</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className='justify-content-end'>
+            <Navbar.Text>
+              Signed in as: <a href='#login'>{this.props.email}</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <hr></hr>
+      </div>
+    )
+  }
+}
